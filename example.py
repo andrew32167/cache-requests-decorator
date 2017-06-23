aa91c4f260e61b663740dcfa4f7096be285aa6e2
Andrey Rimovych <ar@unit.ua>
2017-06-23 17:23:59 +0300

    readme and example updated

--- example.py
+++ example.py
@@ -3,15 +3,17 @@
 from cache_decorator import CacheDecorator
 import requests
 from datetime import datetime
+import time
 
 
 @CacheDecorator
-def get_request(url, **kwargs):
+def delayed_get_request(url, **kwargs):
+    time.sleep(10)
     return requests.get(url, **kwargs)
 
 for t in range(2):
     start = datetime.now()
-    resp = get_request('https://github.com/andrew32167/cache-requests-decorator')
+    resp = delayed_get_request('https://github.com/andrew32167/cache-requests-decorator')
     end = datetime.now()
     print(resp)
     print('Time spent: {}'.format(end - start))
